44a67abd15416c2957fb622d21a52205ca5b1184
SunLangsong <2572070938qq@gmail.com>
2024-05-10 18:12:54 +0900

    Initial commit

---
+++ Assets/MirrorExamplesVR/Scripts/VRHostCameraControl.cs
@@ -0,0 +1,75 @@
+using Mirror;
+using UnityEngine;
+using UnityEngine.InputSystem.XR;
+
+public class VRHostCameraControl : NetworkBehaviour
+{
+    // 在Inspector中指定服务器的摄像机对象
+    public Transform serverCameraTransform;
+    public Transform serverPlayerTransform;
+
+    // 用于存储接收到的数据，可能还需要进一步处理
+    private Vector3 receivedPosition;
+    private Quaternion receivedRotation;
+
+    private GameObject maincamera;
+    private GameObject subcamera;
+    private float nextFrameTime = 0.0f;
+    private float frameRateInterval = 1.0f / 15.0f;
+    [SyncVar]
+    private Vector3 syncedPosition;
+    [SyncVar]
+    private Quaternion syncedRotation;
+    
+    void Start(){
+        maincamera = GameObject.FindWithTag("MainCamera");
+        subcamera = GameObject.FindWithTag("SubCamera");
+    }
+    void Update()
+    {
+        if (isServer)
+        {
+            // 每个更新周期都发送服务器摄像机的当前位置和旋转
+            // Debug.Log("Running as Server.");
+            // Debug.Log(serverCameraTransform);
+            // RpcUpdateClientCamera(serverCameraTransform.position, serverCameraTransform.rotation);
+            //RpcUpdateClientCamera(Camera.main.transform.position, Camera.main.transform.rotation);
+            syncedPosition = maincamera.transform.position;
+            syncedRotation = maincamera.transform.rotation;
+            
+        }
+        if (isClient && !isServer) // 确保这不是主机，主机不需要从自身接收更新
+        {
+            if (Time.time < nextFrameTime)
+            {
+                return;
+                
+            }else{
+                // 在客户端上，将接收到的位置和旋转应用于客户端的主摄像机
+                // Debug.Log(receivedPosition);
+                //Camera.main.transform.parent.position = receivedPosition;
+                //Camera.main.transform.parent.rotation = receivedRotation;
+                maincamera.GetComponent<TrackedPoseDriver>().enabled = false;
+                //Camera.main.transform.parent.position = new Vector3(syncedPosition.x, 0, syncedPosition.z);
+            
+                //Camera.main.transform.parent.rotation = syncedRotation;
+                maincamera.transform.position = new Vector3(syncedPosition.x, syncedPosition.y, syncedPosition.z);
+            
+                maincamera.transform.rotation = syncedRotation;
+
+                //serverPlayerTransform.position = receivedPosition;
+                //serverPlayerTransform.rotation = receivedRotation;
+            }
+            nextFrameTime = Time.time + frameRateInterval;        
+        }
+    }
+
+    [ClientRpc]
+    void RpcUpdateClientCamera(Vector3 position, Quaternion rotation)
+    {
+        // 在所有客户端上接收并存储服务器摄像机的位置和旋转
+        receivedPosition = position;
+        receivedRotation = rotation;
+    }
+    
+}
