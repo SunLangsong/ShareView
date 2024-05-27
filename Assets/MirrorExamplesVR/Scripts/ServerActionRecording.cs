acf0bb85f4d2d675901dc501c1d72e807b1b2d7c
SunLangsong <2572070938qq@gmail.com>
2024-05-27 14:56:07 +0900

    SyncRecord

--- Assets/MirrorExamplesVR/Scripts/ServerActionRecording.cs
+++ Assets/MirrorExamplesVR/Scripts/ServerActionRecording.cs
@@ -3,25 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-public class ServerActionRecording : MonoBehaviour
+using Mirror;
+public class ServerActionRecording : NetworkBehaviour
 {
     public GameObject recordButton;
     public GameObject stopButton;
     public TMP_Text timeText;
     private bool isRecording = false;
     private float recordingTime = 0.0f;
+    private float recordInterval = 1.0f / 60.0f;
+    private float timer;
+    public Transform serverCameraTransform;
+    public Transform Centerposition;
+    [SyncVar]
+    private List<Vector3> camerapositions = new List<Vector3>();
+    [SyncVar]
+    private List<Quaternion> camerarotations = new List<Quaternion>();
+    
 
     void Start()
     {
         recordButton.GetComponent<Button>().onClick.AddListener(ToggleRecording);
         stopButton.GetComponent<Button>().onClick.AddListener(ToggleRecording);
         timeText.text = "0.0s";
+        
     }
 
     void Update()
     {
-        if (isRecording)
+        if (isRecording && isServer)
         {
+            timer += Time.deltaTime;
+
+            // Record the action of the camera as the time interval
+            if (timer >= recordInterval)
+            {
+                camerapositions.Add(serverCameraTransform.position);
+                camerarotations.Add(serverCameraTransform.rotation);
+                timer = 0;
+            }
             recordingTime += Time.deltaTime;
             timeText.text = recordingTime.ToString("F1") + "s";
         }
@@ -30,21 +50,22 @@ public class ServerActionRecording : MonoBehaviour
     void ToggleRecording()
     {
         isRecording = !isRecording;
-        recordingTime = 0;  // 重置录制时间
+        recordingTime = 0;  // Reset the recording time
         if (isRecording)
         {
-            // 更新按钮文本以显示"Stop Recording"
+            // Start to record the action
             recordButton.SetActive(false);
             stopButton.SetActive(true);
-            
-            // 这里可以添加启动录制的代码
+            camerapositions.Clear();
+            camerarotations.Clear();
         }
         else
         {
-            // 更新按钮文本以显示"Start Recording"
+            // Stop to record the action
             recordButton.SetActive(true);
             stopButton.SetActive(false);
-            // 这里可以添加停止录制的代码
+            Debug.Log(camerapositions[1]);
+            
         }
     }
 }
